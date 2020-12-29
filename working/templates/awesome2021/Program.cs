11db5231b0a41ca4ca81d99eccc8471d084bf871
Frandi Dwi <frandi.dwijayanto@polyrific.com>
2020-12-29 13:12:24 +0700

    Init commit

---
+++ working/templates/awesome2021/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace awesome2021
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var wb = new WordsBank();
+
+            await Console.Out.WriteLineAsync(wb.y2020);
+            await Console.Out.WriteLineAsync(wb.y2021);
+        }
+    }
+}
